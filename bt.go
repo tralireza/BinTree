6cfa7d1c4b1132b5ca6691129eeb995ce4c1316f
Alireza <26859725+tralireza@users.noreply.github.com>
2022-03-03 01:57:00 +0000

    Binary Tree 1325m Delete Leaves With a Given Value
    Go

--- bt.go
+++ bt.go
@@ -6,3 +6,22 @@ func init() {
 	log.SetFlags(0)
 	log.SetPrefix("")
 }
+
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+// 1325m Delete Leaves With a Given Value
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	root.Left = removeLeafNodes(root.Left, target)
+	root.Right = removeLeafNodes(root.Right, target)
+	if root.Left == nil && root.Right == nil && root.Val == target {
+		return nil
+	}
+	return root
+}
--- bt_test.go
+++ bt_test.go
@@ -1,7 +1,37 @@
 package BinTree
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"testing"
+)
 
 func init() {
 	log.Print("> Binary Tree")
+}
+
+// 1325m Delete Leaves With a Given Value
+func Test1325(t *testing.T) {
+	var Draw func(*TreeNode)
+	Draw = func(n *TreeNode) {
+		if n != nil {
+			Draw(n.Left)
+			l, r := '*', '*'
+			if n.Left == nil {
+				l = '-'
+			}
+			if n.Right == nil {
+				r = '-'
+			}
+			fmt.Printf("{%c %d %c}", l, n.Val, r)
+			Draw(n.Right)
+		}
+	}
+
+	type T = TreeNode
+	r := &T{1, &T{2, &T{Val: 2}, nil}, &T{3, &T{Val: 2}, &T{Val: 4}}}
+	Draw(r)
+	fmt.Print("  ->  ")
+	Draw(removeLeafNodes(r, 2))
+	fmt.Println()
 }
